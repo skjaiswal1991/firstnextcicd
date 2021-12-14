c994d8ed34f0e15e6106ba6b33476f1d6babc564
Sonu <sonu@Sonus-MacBook-Air.local>
2021-12-14 21:17:49 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! App
+          Welcome to <a href="https://nextjs.org">Next.js! A\ Sanjaypp
           </a>
         </h1>
 
